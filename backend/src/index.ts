aecb986401a51405c6cabf1aa99b22013168e47b
Subahanumanth <subahanu1999@gmail.com>
2024-10-14 19:39:47 +0530

    Add cors to allow local client app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://summary-scoop.onrender.com",
+    origin: "http://localhost:5173",
     methods: ["POST"],
   })
 );
